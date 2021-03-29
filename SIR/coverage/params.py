fab199c3591acc3fbc307efad7c8a9e60c75a81c
Thayer Fisher <tfisher2@quokka.fhcrc.org>
2021-03-29 14:23:33 -0700

    add SIR big sim

--- SIR/coverage/params.py
+++ SIR/coverage/params.py
@@ -16,13 +16,13 @@ test_n = 250
 # Testing Batch Size
 t_batch_size=2000
 # Learning rate
-step_size = 0.00001
+step_size = 0.00005
 # Learning rate decay
 decay_size = .0001
 # Number of steps per epoch
 steps_per_epoch = 1
 # Number of epochs
-num_epochs = 5000
+num_epochs = 1000
 # Number of epochs to run before starting model averaging
 burn_in = 100.0
 
